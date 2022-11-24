de7801988038ed9dc50c568917ea2a4ad46e9725
chipcasla <103225088+chipcasla@users.noreply.github.com>
2022-11-24 15:16:34 -0300

    Delete Ej5AccesoADatos directory

--- Ej5AccesoADatos/db/userJava.sql
+++
@@ -1,6 +0,0 @@
---
--- User java
---
-
-create user 'java'@'%' identified by 'himitsu';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `java`.* TO 'java'@'%';
--- Ej5AccesoADatos/src/ui/Principal.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-public class Principal {
-
-	public static void main(String[] args) {
-		Menu m=new Menu();
-		m.start();
-	}
-}
